5ab1ef8f0db37ca2f88536c88af252f55ce8e961
USERSAD\jaloncan <joseluji2234@gmail.com>
2023-01-19 16:52:24 +0100

    crear clases model, mapeo clases a tablas con JPA, integrado el diseño y vistas

--- src/main/java/com/curso/ecommerce/model/Producto.java
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -1,7 +1,17 @@
 package com.curso.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
@@ -9,6 +19,8 @@ public class Producto {
 	private double precio;
 	private int cantidad;
 	
+	@ManyToOne
+	private Usuario usuario;
 	
 	
 	
@@ -16,7 +28,11 @@ public class Producto {
 	}
 
 
-	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+	
+
+
+	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -24,7 +40,11 @@ public class Producto {
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
+
+
+
 
 
 	public Integer getId() {
@@ -92,6 +112,19 @@ public class Producto {
 		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen
 				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
 	}
+
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+
+	
 	
 	
 	
